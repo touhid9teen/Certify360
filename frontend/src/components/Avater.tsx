977a15f54b617ab4a0bd3a6e654dae12f81e2e4a
touhid9teen <toouhidul@gmail.com>
2025-01-22 00:42:22 +0600

    Avate component is updated

--- frontend/src/components/Avater.tsx
+++ frontend/src/components/Avater.tsx
@@ -1,44 +1,38 @@
+import React from 'react';
 import { getInitials } from '../utils/initials';
 
 interface AvatarProps {
-    circle?: boolean;
-    image?: string;
-    alt?: string;
-    fullName?: string;
-    divCustomClass?: string;
-    imgCustomClass?: string;
-    nameCustomClass?: string;
+  circle?: boolean;
+  image?: string;
+  alt?: string;
+  fullName?: string;
+  divCustomClass?: string;
+  imgCustomClass?: string;
+  nameCustomClass?: string;
 }
 
-const Avatar: React.FC<AvatarProps> = (props: AvatarProps) => {
-    const {
-        circle = false,
-        image = '',
-        alt = 'avatar',
-        fullName = 'AV',
-        divCustomClass = 'w-11 h-11 items-end',
-        imgCustomClass = 'w-10 h-10',
-        nameCustomClass = '',
-    } = props;
-
-    // Use a dummy avatar image if no image is provided
-    const defaultAvatar = 'https://randomuser.me/api/portraits/men/20.jpg';
-// Dummy avatar
-    const avatarImage = image.length > 0 ? image : defaultAvatar;
-
-    return (
-        <div
-            className={`flex justify-center bg-[#F5F5F5] ${divCustomClass} ${
-                circle === true ? 'rounded-full' : 'rounded-lg'
-            }`}
-        >
-            <img
-                src={avatarImage}
-                alt={alt}
-                className={`text-xm ${imgCustomClass}`}
-            />
-        </div>
-    );
+const Avatar: React.FC<AvatarProps> = ({
+  circle = false,
+  image = '',
+  alt = 'avatar',
+  fullName = 'AV',
+  divCustomClass = 'w-11 h-11 items-end',
+  imgCustomClass = 'w-10 h-10',
+  nameCustomClass = '',
+}: AvatarProps) => {
+  return (
+    <div
+      className={`flex justify-center bg-[#F5F5F5] ${divCustomClass} ${
+        circle === true ? 'rounded-full' : 'rounded-lg'
+      }`}
+    >
+      {image.length > 0 ? (
+        <img src={image} alt={alt} className={`text-xs ${imgCustomClass}`} />
+      ) : (
+        <span className={`text-deep-blue font-bold ${nameCustomClass}`}>{getInitials(fullName)}</span>
+      )}
+    </div>
+  );
 };
 
 export default Avatar;
